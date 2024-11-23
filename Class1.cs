de36c6873066f3b4beaefaf5f316584f6725b94b
rayyanmohsin1 <rmohsin@my.centennialcollege.ca>
2024-11-23 15:48:48 +0500

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace lab_4
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpperCase(this string input)
+        {
+            if (string.IsNullOrWhiteSpace(input)) return false;
+            char firstChar = input[0];
+            return char.IsUpper(firstChar);
+        }
+    }
+
+}
